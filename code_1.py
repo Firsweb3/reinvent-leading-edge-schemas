69a744c2b93fed0c4643ae67462e766b0d43ae01
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-09-21 11:00:31 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,7 +1,7 @@
 
 def generate_random_data():
-    random_string = 'Their power field TV.'
-    random_number = 89
+    random_string = 'Today Republican range though board head.'
+    random_number = 26
 
     data = [(random_string, random_number) for _ in range(10)]
     return data
@@ -10,8 +10,8 @@ def main():
     data = generate_random_data()
     for item in data:
         random_string, random_number = item
-        print(f"Random String: Their power field TV.")
-        print(f"Random Number: 89")
+        print(f"Random String: Today Republican range though board head.")
+        print(f"Random Number: 26")
 
 if __name__ == "__main__":
     main()
